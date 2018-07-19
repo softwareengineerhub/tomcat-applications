94afd0fba3c3c488ec6213978ecf88b7bcd85d0b
denis4321 <denis4321@ukr.net>
2018-07-19 14:53:24 +0300

    JAX-RS another jersey dependency

---
+++ tomcat-web7-jaxrs2/src/main/java/com/mycompany/tomcat/web7/jaxrs2/GenericResource.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tomcat.web7.jaxrs2;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+/**
+ * REST Web Service
+ *
+ * @author denys.prokopiuk
+ */
+@Path("generic")
+public class GenericResource {
+
+    @GET
+    public String getXml() {
+        return "Test";
+    }
+
+}

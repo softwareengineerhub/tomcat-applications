c40247384e304013f94278d18bb3a68aa75f8dcc
denis4321 <denis4321@ukr.net>
2018-08-01 14:42:29 +0300

    Tomcat annotation based security

---
+++ tomcat-security-annotations/src/main/java/com/mycompany/tomcat/security/annotations/UserServlet.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tomcat.security.annotations;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.HttpConstraint;
+import javax.servlet.annotation.HttpMethodConstraint;
+import javax.servlet.annotation.ServletSecurity;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author denys.prokopiuk
+ */
+@WebServlet(name = "UserServlet", urlPatterns = {"/UserServlet"})
+@ServletSecurity(@HttpConstraint(rolesAllowed = {"admin-app", "user-app"}))
+public class UserServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().append("User");
+    }
+
+}
